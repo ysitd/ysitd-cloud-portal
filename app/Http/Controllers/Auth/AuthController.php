7c0873b5f4c8b5bfe414e2f376a2887b5e79e6d7
Tony Yip <tony@opensource.hk>
2016-08-13 22:09:37 +0800

    Remove useless method and controller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,61 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\Validator;
 use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\Controller;
+use Ramsey\Uuid\Uuid;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
-
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
 
     /**
      * Route for github oauth
@@ -99,10 +50,15 @@ class AuthController extends Controller
         }
     }
 
+    /**
+     * @param Guard $auth
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function signin(Guard $auth)
     {
         if (env('APP_ENV') === 'dev') {
-            $auth->loginUsingId('00000000-0000-0000-0000-000000000000');
+            $auth->loginUsingId(Uuid::NIL);
             return redirect()->route('home');
         } else {
             return redirect()->route('auth.github.oauth');
