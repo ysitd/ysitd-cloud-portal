45b1257d693eeaba756d1b5576851829c03d2df7
Tony Yip <tony@opensource.hk>
2016-08-15 04:39:37 +0800

    Create Issue display controller and model

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Issue\CreateRequest;
 use App\Models\Issue;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 class IssueController extends Controller
 {
@@ -18,8 +21,37 @@ class IssueController extends Controller
         return view('issue/create', ['title' => 'Report Issue']);
     }
 
-    public function store(CreateRequest $request)
+    public function store(CreateRequest $request, Guard $auth)
     {
+        $data = [
+            'owner' => $auth->user()->user_id,
+            'detail' => $request->input('detail'),
+            'category' => $request->input('issue_type'),
+            'service' => $request->input('service'),
+            'title' => $request->input('title')
+        ];
 
+        Issue::create($data)->save();
+        return redirect()->route('issue.index');
+    }
+
+    public function show($issue, Request $request)
+    {
+        try {
+            $issue = Issue::findOrFail($issue);
+            if ($request->ajax() || $request->wantsJson()) {
+                return response()->json([
+
+                ]);
+            } else {
+                return view('issue/show', ['title' => 'View Issue', 'issue' => $issue]);
+            }
+        } catch (ModelNotFoundException $e) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response()->json(['error' => 'Not exists']);
+            } else {
+                return redirect()->route('issue.index');
+            }
+        }
     }
 }
--- app/Models/Issue.php
+++ app/Models/Issue.php
@@ -6,8 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Issue extends Model
 {
+
+    protected $fillable = [
+        'title', 'owner',
+        'service', 'category', 'detail'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function comments()
     {
         return $this->hasMany(IssueComment::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'owner', 'user_id');
     }
 }
